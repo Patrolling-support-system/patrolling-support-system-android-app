f40f696d05e1b79493a9a432a82ab6f454119e42
GrzegorzPlo <gplociennik@student.agh.edu.pl>
2023-03-03 20:48:01 +0100

    Firebase auth

--- app/src/main/java/pl/agh/patrollingsupportsystem/activities/LoginActivity.java
+++ app/src/main/java/pl/agh/patrollingsupportsystem/activities/LoginActivity.java
@@ -1,27 +1,67 @@
 package pl.agh.patrollingsupportsystem.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import pl.agh.patrollingsupportsystem.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etLoginEmail;
+    EditText etLoginPassword;
+    Button btn;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btn = (Button)findViewById(R.id.loginButton);
+        etLoginEmail = findViewById(R.id.editTextEmailAddress);
+        etLoginPassword = findViewById(R.id.editTextPassword);
+        btn = (Button)findViewById(R.id.loginButton);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+        mAuth = FirebaseAuth.getInstance();
+
+        btn.setOnClickListener(v -> {
+            //startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+            String email = etLoginEmail.getText().toString();
+            String password = etLoginPassword.getText().toString();
+
+            if (TextUtils.isEmpty(email)){
+                etLoginEmail.setError("Email cannot be empty");
+                etLoginEmail.requestFocus();
+            }else if (TextUtils.isEmpty(password)){
+                etLoginPassword.setError("Password cannot be empty");
+                etLoginPassword.requestFocus();
+            }else{
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
---
+++ app/src/main/java/pl/agh/patrollingsupportsystem/activities/MainActivity.java
@@ -0,0 +1,32 @@
+package pl.agh.patrollingsupportsystem.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import pl.agh.patrollingsupportsystem.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+}
