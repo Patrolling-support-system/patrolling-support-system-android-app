e2465fa079b4604a9c24c24b120accff2c4e4721
GrzegorzPlo <gplociennik@student.agh.edu.pl>
2023-03-11 19:24:35 +0100

    OnClick for element and filter for action list

--- app/src/main/java/pl/agh/patrollingsupportsystem/activities/ActionsListActivity.java
+++ app/src/main/java/pl/agh/patrollingsupportsystem/activities/ActionsListActivity.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -16,12 +19,14 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import pl.agh.patrollingsupportsystem.R;
 import pl.agh.patrollingsupportsystem.recyclerViewProperties.ActionGeneral;
 import pl.agh.patrollingsupportsystem.recyclerViewProperties.ActionListAdapter;
+import pl.agh.patrollingsupportsystem.recyclerViewProperties.RecyclerViewInterface;
 
-public class ActionsListActivity extends AppCompatActivity {
+public class ActionsListActivity extends AppCompatActivity implements RecyclerViewInterface {
 
     RecyclerView recyclerView;
     FirebaseFirestore db;
@@ -41,7 +46,7 @@ public class ActionsListActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         list = new ArrayList<>();
-        actionListAdapter = new ActionListAdapter(this, list);
+        actionListAdapter = new ActionListAdapter(this, list, this);
         recyclerView.setAdapter(actionListAdapter);
 
         EventChangeListener();
@@ -50,7 +55,9 @@ public class ActionsListActivity extends AppCompatActivity {
     }
 
     private void EventChangeListener() {
-        db.collection("ActionList").orderBy("actionName", Query.Direction.ASCENDING)
+        db.collection("ActionList")
+                .whereArrayContains("members", FirebaseAuth.getInstance().getUid())
+                //.orderBy("actionName", Query.Direction.ASCENDING) //Temporary not needed and requires index
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -67,6 +74,12 @@ public class ActionsListActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Toast t = Toast.makeText(this, FirebaseAuth.getInstance().getCurrentUser().getUid(), Toast.LENGTH_LONG);
+        t.show();
     }
 }
 
