d13b845de9628a5ecc3ea82c8d4d1134c2b124fd
GrzegorzPlo <gplociennik@student.agh.edu.pl>
2023-03-11 20:38:48 +0100

    User details activity implementation

---
+++ app/src/main/java/pl/agh/patrollingsupportsystem/activities/ActionDetailsActivity.java
@@ -0,0 +1,16 @@
+package pl.agh.patrollingsupportsystem.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.agh.patrollingsupportsystem.R;
+
+public class ActionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_details);
+    }
+}
