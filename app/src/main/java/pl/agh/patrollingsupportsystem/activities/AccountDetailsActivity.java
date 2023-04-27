17b6430967b070193de36cf01c77dec83073a27c
GrzegorzPlo <gplociennik@student.agh.edu.pl>
2023-04-27 19:35:03 +0200

    Account Details Activity and Layout refactoring

--- app/src/main/java/pl/agh/patrollingsupportsystem/activities/AccountDetailsActivity.java
+++ app/src/main/java/pl/agh/patrollingsupportsystem/activities/AccountDetailsActivity.java
@@ -2,7 +2,6 @@ package pl.agh.patrollingsupportsystem.activities;
 
 import static android.content.ContentValues.TAG;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -12,64 +11,64 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import pl.agh.patrollingsupportsystem.R;
 
 public class AccountDetailsActivity extends AppCompatActivity {
 
-    TextView userName, userSurname, userEmailAddress;
-    FirebaseFirestore db;
+    TextView tvName;
+    TextView tvSurname;
+    TextView tvEmailAddress;
+    Button btnChangePassword;
+    FirebaseFirestore fbDb;
+    FirebaseUser fbAuth;
+
+    String userId;
+    String userEmailAddress;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_account);
+        setContentView(R.layout.activity_account_details);
 
-        userName = findViewById(R.id.userName);
-        userSurname = findViewById(R.id.userSurname);
-        userEmailAddress = findViewById(R.id.userEmailAddress);
-        Button changePasswordButton = findViewById(R.id.changePasswordButton);
+        //Layout elements
+        tvName = findViewById(R.id.textViewName);
+        tvSurname = findViewById(R.id.textViewSurname);
+        tvEmailAddress = findViewById(R.id.textViewEmailAddress);
+        btnChangePassword = findViewById(R.id.buttonChangePassword);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirebaseUser authUser = FirebaseAuth.getInstance().getCurrentUser();
-        String userId = authUser.getUid();
-        String userEmailAddressText = authUser.getEmail();
-        Task a = authUser.updatePassword("TEST");
+        //Firebase instance and auth
+        fbDb = FirebaseFirestore.getInstance();
+        fbAuth = FirebaseAuth.getInstance().getCurrentUser();
 
-        CollectionReference collectionRef = db.collection("User");
-        Query query = collectionRef.whereEqualTo("userId", userId);
-        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
+        //User information
+        userId = fbAuth.getUid();
+        userEmailAddress = fbAuth.getEmail();
 
-                        String name = document.getString("name");
-                        userName.setText(name);
+        //Values for fields
+        tvEmailAddress.setText(userEmailAddress);
 
-                        String surname = document.getString("surname");
-                        userSurname.setText(surname);
-                    }
-                } else {
-                    Log.d(TAG, "Error getting user document: ", task.getException());
+        //User collection query and values for fields
+        fbDb.collection("User").whereEqualTo("userId", userId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                    String name = document.getString("name");
+                    tvName.setText(name);
+
+                    String surname = document.getString("surname");
+                    tvSurname.setText(surname);
                 }
+            } else {
+                Log.d(TAG, "Error getting user document: ", task.getException());
             }
         });
 
-        userEmailAddress.setText(userEmailAddressText);
-
-        changePasswordButton.setOnClickListener(v ->{
-            startActivity(new Intent(AccountDetailsActivity.this, ChangePasswordActivity.class));
-        });
+        btnChangePassword.setOnClickListener(v -> startActivity(new Intent(AccountDetailsActivity.this, ChangePasswordActivity.class)));
     }
 }
